4f48e6d24b3d0d622506761374d9042aa8bafbb2
jchrisfay <chrisfaycontact@gmail.com>
2020-11-06 17:07:16 +0000

    changed dice game to make more sense, values are  put in a dictionary,  if/elif checks if its value is the smallest. probably not the  most effective method..

--- mini_programs/dice.py
+++ mini_programs/dice.py
@@ -2,35 +2,40 @@
 
 from random import randrange
 
-limit = 1000
+limit = 100000
 rolled = 0
 firstWins = 0
 secondWins = 0
 thirdWins = 0
+fourthWins = 0
+fifthWins = 0
 draw = 0
+
+
 
 while rolled < limit:
 
     first = (randrange(6)+randrange(6))
     second = (randrange(6)+randrange(6))
     third = (randrange(6)+randrange(6))
-
-
-    if third < first > second:
-        firstWins = firstWins + 1
-
-    elif first < second > third:
-        secondWins = secondWins + 1
-
-    elif first < third > second:
-        thirdWins = thirdWins + 1
-
-    else:
-        draw = draw + 1
-
+    fourth = (randrange(6)+randrange(6))
+    fifth = (randrange(6)+randrange(6))
+    rolls = {"1st":first,"2nd":second,"3rd":third,"4th":fourth,"5th":fifth}
     rolled = rolled + 1
 
-print('First wins ' + str(firstWins))
-print('Second wins ' + str(secondWins))
-print('Third wins ' + str(thirdWins))
-print('Draw ' + str(draw))
+    if min(rolls, key=rolls.get) == "1st":
+        firstWins = firstWins + 1
+    elif min(rolls, key=rolls.get) == "2nd":
+        secondWins = secondWins + 1
+    elif min(rolls, key=rolls.get) == "3rd":
+        thirdWins = thirdWins + 1
+    elif min(rolls, key=rolls.get) == "4th":
+        fourthWins = fourthWins + 1
+    elif min(rolls, key=rolls.get) == "5th":
+        fifthWins = fifthWins + 1
+
+print("First lose :" + str(firstWins))
+print("Second lose :" + str(secondWins))
+print("Third lose :" + str(thirdWins))
+print("Fourth lose :" + str(fourthWins))
+print("Fifth lose :" + str(fifthWins))
